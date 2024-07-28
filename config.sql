4e5043489684a8a43138e349bc81aeac6e55c173
whitneyyuen <whitneyuen@yahoo.com.hk>
2024-07-28 00:22:57 -0400

    - add column for dim_customer

--- config.sql
+++ config.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE cics_database;
 GO
 USE cics_database;
-CREATE TABLE dim_customer (customer_id varchar(50) PRIMARY KEY, first_name nvarchar(255) NOT NULL, last_name nvarchar(255) NOT NULL);
-INSERT INTO dim_customer VALUES ('0', 'Anson', 'Ng');
-INSERT INTO dim_customer VALUES ('1', 'Stephanie', 'Yip');
-INSERT INTO dim_customer VALUES ('2', 'Timothy', 'Wong');
-INSERT INTO dim_customer VALUES ('3', 'Whitney', 'Yuen');
-INSERT INTO dim_customer VALUES ('4', 'Winnie', 'Choi');
+CREATE TABLE dim_customer (customer_id varchar(50) PRIMARY KEY, first_name nvarchar(255) NOT NULL, last_name nvarchar(255) NOT NULL, GENDER nvarchar(255) NOT NULL, DOB date /*NOT NULL*/, ADDRESS nvarchar(100) /*NOT NULL*/, CITY nvarchar(50) /*NOT NULL*/, POSTAL_CODE varchar(7) /*NOT NULL*/, PHONE_NO bigint /*NOT NULL*/, EMAIL varchar(100), CREDIT_BALANCE decimal DEFAULT 0);
+INSERT INTO dim_customer (customer_id, first_name, last_name,GENDER) VALUES ('1', 'Anson', 'Ng', 'M');
+INSERT INTO dim_customer (customer_id, first_name, last_name,GENDER) VALUES ('2', 'Stephanie', 'Yip' ,'F');
+INSERT INTO dim_customer (customer_id, first_name, last_name,GENDER) VALUES ('3', 'Timothy', 'Wong', 'M');
+INSERT INTO dim_customer (customer_id, first_name, last_name,GENDER) VALUES ('4', 'Whitney', 'Yuen', 'F');
+INSERT INTO dim_customer (customer_id, first_name, last_name,GENDER) VALUES ('5', 'Winnie', 'Choi', 'F');
