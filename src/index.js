1bce793cea92699eba3c42be37c948cfe0f6bdf0
whitneyyuen <whitneyuen@yahoo.com.hk>
2024-08-04 00:43:49 -0400

    - try to fix cors problem

--- src/index.js
+++ src/index.js
@@ -11,6 +11,14 @@ import courseRouter from "./router/CourseRouter.js";
 
 const app = express();
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
 
 databaseConnection(app);
 app.use('/api/v1/*', authentication);
