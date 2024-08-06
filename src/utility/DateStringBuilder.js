8d90d5baeff14d061c97ac133a4e4610bf28593e
ansonngg <ngkawai9886@gmail.com>
2024-08-06 19:53:18 -0400

    Fix dates trim time bug

--- src/utility/DateStringBuilder.js
+++ src/utility/DateStringBuilder.js
@@ -1,10 +1,14 @@
 ﻿export function trimTime(dates) {
+    if (!dates) {
+        return null;
+    }
+
     if (!Array.isArray(dates)) {
-        return dates.slice(0, 10);
+        return dates.toISOString().slice(0, 10);
     }
 
     dates.forEach((date, index) => {
-        dates[index] = date.slice(0, 10);
+        dates[index] = date.toISOString().slice(0, 10);
     });
 
     return dates;
